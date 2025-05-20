f177354596d407491c69a4d80624dc4e8363db79
Jan-Erik Rediger <janerik@fnordig.de>
2025-05-20 11:11:59 +0200

    Add MPL license header to source files
    Fixes #57

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use mdbook::book::{Book, BookItem, Chapter};
 use mdbook::errors::Result;
 use mdbook::preprocess::{Preprocessor, PreprocessorContext};
